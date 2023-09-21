3de90388798a0f2183b7fa891778d5720af8768f
ywcodey <ykgray0@gmail.com>
2023-09-21 21:19:12 +0800

    add RestAPI and connect to MongoDB

--- backend/controllers/memoController.js
+++ backend/controllers/memoController.js
@@ -1,8 +1,12 @@
 const asyncHandler = require("express-async-handler");
 
+const Memo = require("../models/memoModel");
+
 // Get memos
 const getMemos = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get memos" });
+  const memos = await Memo.find();
+
+  res.status(200).json(memos);
 });
 
 // Create memo
@@ -11,17 +15,42 @@ const createMemo = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please add a text field");
   }
-  res.status(200).json({ message: "Create memos" });
+
+  const memo = await Memo.create({
+    text: req.body.text,
+  });
+
+  res.status(200).json(memo);
 });
 
 // Update memo
 const updateMemo = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update memo ${req.params.id}` });
+  const memo = await Memo.findById(req.params.id);
+
+  if (!memo) {
+    res.status(400);
+    throw new Error("Memo not found");
+  }
+
+  const updatedMemo = await Memo.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedMemo);
 });
 
 // Delete memo
 const deleteMemo = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete memo ${req.params.id}` });
+  const memo = await Memo.findById(req.params.id);
+
+  if (!memo) {
+    res.status(400);
+    throw new Error("Memo not found");
+  }
+
+  await Memo.findByIdAndRemove(req.params.id);
+
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
